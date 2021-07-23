e1010d65380554620e511fc4a0a832fe75cee2fc
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2021-07-24 00:12:11 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -16,29 +16,3 @@ def update():
 
 update()
 tk.mainloop()
-
-
-import tkinter
-from tkinter import Label, Tk
-from time import strftime
-from PIL import ImageTk
-
-tk =Tk()
-root= tkinter.Tk()
-tk.title("BIG BEN")
-tk.configure(bg="white")
-
-widget = tkinter.Canvas(root, width=600, height = 300)
-widget.grid(columnspan=3)
-
-widget= Label(widget, font=("Arial",30,"bold"), bg="gray", fg="white")
-widget.grid(column=1, row =1)
-
-
-def update():
-    time = strftime("%H :%M :%S %pm")
-    widget.configure(text = time)
-    widget.after(80, update)
-
-update()
-tk.mainloop()
